8c1150cc06fe98b3bcaff1e905d8b939c27d5636
Iyanu Oluwa <iyanu1396@gmail.com>
2024-06-09 22:36:05 +0100

    fixed nav text

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -8,7 +8,7 @@ function Nav() {
   return (
     <Link to={isLoggedIn ? `/menu` : `/`}>
       <nav className="top-0 sticky flex  cursor-pointer items-center justify-center bg-stone-200 gap-3 z-50 border-b-2 border-green-500 py-6 font-bebas text-4xl text-green-950 sm:text-5xl">
-        <h1>Spot Lists</h1>
+        <h1>Stats Lists</h1>
         <i className="fa-brands fa-spotify text-green-500 "></i>
       </nav>
     </Link>
